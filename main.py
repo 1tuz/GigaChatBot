785a4e230dda425fd290cc9a4e1a027084aafc7e
1tuz <creameanwoner@gmail.com>
2023-01-22 16:42:09 +0300

    added readme

---
+++ main.py
@@ -0,0 +1,31 @@
+import openai 
+import os
+from dotenv import load_dotenv
+from aiogram import Bot, types 
+from aiogram.dispatcher import Dispatcher 
+from aiogram.utils import executor 
+
+load_dotenv()
+
+token = os.getenv('TOKEN') 
+openai.api_key = os.getenv('OPENAI_KEY') 
+
+bot = Bot(token) 
+dp = Dispatcher(bot) 
+
+@dp.message_handler() 
+async def send(message : types.Message): 
+    response = openai.Completion.create( 
+        model="text-davinci-002", 
+        prompt=message.text, 
+        temperature=0.9, 
+        max_tokens=1000, 
+        top_p=1.0, 
+        frequency_penalty=0.0, 
+        presence_penalty=0.6, 
+        stop=["You:"] 
+    ) 
+    # Send the AI's response as a reply
+    await message.reply(response['choices'][0]['text'])
+
+executor.start_polling(dp, skip_updates=True)
